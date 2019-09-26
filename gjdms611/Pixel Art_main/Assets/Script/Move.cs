03eb162a19212058343606f014b065dcd1841020
gjdms611 <gjdms611@naver.com>
2019-09-26 22:36:07 +0900

    commit

---
+++ gjdms611/Pixel Art_main/Assets/Script/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    void move_object_circle(int num, Gameobject obj)
+    {
+        Vector3 tmp = obj.transform.position;
+        tmp.x = Mathf.Cos(-Time.time + num) * 10;
+        tmp.y = Mathf.Sin(-Time.time + num) * 10;
+        obj.transform.position = tmp;
+    }
+    /*
+    int num = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update() {
+        Vector3 tmp = this.transform.position;
+        tmp.x = Mathf.Cos(-Time.time + num) * 10;
+        tmp.y = Mathf.Sin(-Time.time + num) * 10;
+        this.transform.position = tmp;
+        //        double x = tmp.x, y = tmp.y;
+
+        //float xMove = Mathf.Cos(Time.time)/4; //x축으로 이동할 양
+        //float yMove = Mathf.Sin(Time.time)/4; //y축으로 이동할양
+        //this.transform.Translate(new Vector3(xMove, yMove, 0));  //이동
+    }*/
+}
---
+++ gjdms611/Pixel Art_main/Assets/Script/control.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    int num = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move.move_object_circle(num, Gameobject);
+    }
+}
