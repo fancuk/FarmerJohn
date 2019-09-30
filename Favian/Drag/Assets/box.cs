dded0c3e96a07eec5c011b1b13f0883199aeea2f
Favian <damin8@naver.com>
2019-09-30 20:33:13 +0900

    mouse drag
    mouse drag 기능
    
    어느 지점에서든 마우스로 drag 가능
    
    to do
    
    특정 scene 안에서 행성 drag, 오른쪽 버튼 클릭 시 오른쪽 행성 or drag하면 그쪽 행성 줌인

---
+++ Favian/Drag/Assets/box.cs
@@ -0,0 +1,42 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float startPosx;
+    private float startPosY;
+    private bool isBeingHeId = false;
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            startPosx = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+            isBeingHeId = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeId = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeId == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.gameObject.transform.localPosition
+                = new Vector3(mousePos.x - startPosx, mousePos.y - startPosY, 0);
+        }
+    }
+}
