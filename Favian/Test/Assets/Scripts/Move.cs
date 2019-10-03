4941f7744ae544b29c36f6cdf8e9badca9f82229
Favian <damin8@naver.com>
2019-10-03 22:01:22 +0900

    2019-10-03
    지금까지 행성들 만들고
    
    버튼으로 행성이동 x축 y축 3,-3으로 (아직까지는)
    
    드래그 가능(기준 잡아서 그 기준안에서만 드래그 가능하게 만들어야 함)
    
    현재 태양(sun) object에만 적용 했습니다.

---
+++ Favian/Test/Assets/Scripts/Move.cs
@@ -0,0 +1,52 @@
+﻿using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float startPosx;
+    private float startPosY;
+    private bool isBeingHeId = false;
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            startPosx = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+            isBeingHeId = true;
+        }
+    }
+    private void OnMouseUp()
+    {
+        isBeingHeId = false;
+    }
+    private void isBeing()
+    {
+        if (isBeingHeId == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.gameObject.transform.localPosition
+                = new Vector3(mousePos.x - startPosx, mousePos.y - startPosY, 0);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        isBeing();
+    }
+    public void leftDown()
+    {
+        transform.Translate(-3, 3, 0);
+    }
+    public void rightDown()
+    {
+        transform.Translate(3, -3, 0);
+    }
+
+}
